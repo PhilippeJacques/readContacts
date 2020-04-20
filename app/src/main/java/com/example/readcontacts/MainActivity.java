573d15c62cd2c962e9146f28a4454aadb3ca312c
Jacques <pepephilippe287@gmail.com>
2020-04-20 03:50:06 +0200

    third commit

--- app/src/main/java/com/example/readcontacts/MainActivity.java
+++ app/src/main/java/com/example/readcontacts/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.readcontacts;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
@@ -16,15 +18,18 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private Adapter adapter;
     ImageView image;
-    TextView tv_phonebook;
-    ArrayList<String> arraylist;
+    //TextView tv_phonebook;
+    ArrayList<String> Name = new ArrayList<>();
+    ArrayList<String> Mobile = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv_phonebook = findViewById(R.id.tv_phonebook);
-        arraylist = new ArrayList<>();
+
+        //image =(ImageView) findViewById(R.id.imageContact);
 
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED){
             requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
@@ -32,17 +37,22 @@ public class MainActivity extends AppCompatActivity {
         }else{
             getcontact();
         }
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Adapter(this, Name, Mobile);
+        recyclerView.setAdapter(adapter);
     }
     private void getcontact(){
         Cursor cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
                 null, null, null, null);
         while (cursor.moveToNext()){
             String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-            String mobile = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            String mobile = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));/// voici sa
 
-
-            arraylist.add(name +"\n" + mobile+ "\n");
-            tv_phonebook.setText(arraylist.toString());
+           Name.add(name);
+           Mobile.add(mobile);// array with the name and the numbers
+            //tv_phonebook.setText(arraylist.toString());
 
         }
     }
